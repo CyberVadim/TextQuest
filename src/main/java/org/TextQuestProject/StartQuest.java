1357e05d81db12c626e2eddd261c3cc9eed1e45d
”Vadim <protasoff61@gmail.com>
2024-11-17 02:53:38 +0400

    1

---
+++ src/main/java/org/TextQuestProject/StartQuest.java
@@ -0,0 +1,9 @@
+package org.TextQuestProject;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value = "/start")
+public class StartQuest extends HttpServlet {
+
+}
